228a1493d4dc84065da8eaaee18b312d51f3ecf6
tuyrk <tuyrk@qq.com>
2020-07-20 16:54:12 +0800

    第2-3课：抽象类和接口——附面试题

---
+++ java-interview/interview-code/src/main/java/com/interview/Lesson2_3.java
@@ -0,0 +1,70 @@
+package com.interview;
+
+/**
+ * 抽象类和接口的应用 + 面试题
+ */
+public class Lesson2_3 {
+}
+
+abstract class AbstractAnimal {
+    public AbstractAnimal() {
+        System.out.println("Init AbstractAnimal.");
+    }
+
+    protected static String animalName = "AbstractAnimal";
+
+    abstract void eat();
+
+    public void run() {
+        System.out.println("AbstractAnimal Run.");
+    }
+}
+
+class AbstractAnimalInstance extends AbstractAnimal {
+    public static void main(String[] args) {
+        AbstractAnimal animal = new AbstractAnimalInstance();
+        animal.run();
+        System.out.println(animalName);
+        animal.eat();
+    }
+
+    @Override
+    void eat() {
+        System.out.println("AbstractAnimalInstance Eat.");
+    }
+}
+
+//@FunctionalInterface
+interface IAnimal {
+    static String animalName = "Animal Name";
+
+    static void printSex() {
+        System.out.println("Male Dog");
+    }
+
+    default void printAge() {
+        System.out.println("18");
+    }
+
+    void run(String name);
+}
+
+//class FunctionInterfaceTest {
+//    public static void main(String[] args) {
+//        IAnimal animal = name -> System.out.println(name);
+//        animal.run("WangWang");
+//    }
+//}
+
+class AnimalImpl implements IAnimal {
+    static String animalName = new String("Animal Name");
+
+    public static void main(String[] args) {
+        System.out.println(IAnimal.animalName == animalName);
+    }
+
+    @Override
+    public void run(String name) {
+        System.out.println("AnimalImpl：" + name);
+    }
+}
