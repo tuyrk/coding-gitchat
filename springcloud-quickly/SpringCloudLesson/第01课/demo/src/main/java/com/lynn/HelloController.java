7dcae7e506b3a0fd928e345894e6f0fee4306a24
tuyrk <tuyrk@qq.com>
2020-08-04 14:46:33 +0800

    第01课：Spring Boot 入门

--- springcloud-quickly/SpringCloudLesson/第01课/demo/src/main/java/com/lynn/HelloController.java
+++ springcloud-quickly/SpringCloudLesson/第01课/demo/src/main/java/com/lynn/HelloController.java
@@ -4,12 +4,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloController{
-
+public class HelloController {
     @RequestMapping("hello")
     String hello() {
         return "Hello World!";
     }
-
-
 }
